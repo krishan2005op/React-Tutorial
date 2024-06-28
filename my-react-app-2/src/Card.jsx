3605729f1d861a2297f9585c79eef4d2560f0f08
krishan2005op <krishanmalhotra2005@gmail.com>
2024-06-28 12:51:06 +0530

    bug fixes

---
+++ my-react-app-2/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Krisha Malhotra</h2>
+            <p>I am a Programmer and like to play video games</p>
+        </div>
+    );
+}
+
+export default Card
