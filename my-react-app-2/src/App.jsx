879d150734caefc1a83bdc9c591437822e39a28d
krishan2005op <krishanmalhotra2005@gmail.com>
2024-06-29 13:34:33 +0530

    updated

--- my-react-app-2/src/App.jsx
+++ my-react-app-2/src/App.jsx
@@ -1,8 +1,10 @@
-
+import Card from "./Card.jsx"
 
 function App() {
   
-
+  return(
+    <Card/>
+  );
   
 }
 
--- my-react-app-2/src/Card.jsx
+++ my-react-app-2/src/Card.jsx
@@ -2,7 +2,7 @@
 function Card(){
     return(
         <div className="card">
-            <img src="" alt="" />
+            <img src="https://via.placeholder.com/150" alt="Profile Picture" />
             <h2>Krisha Malhotra</h2>
             <p>I am a Programmer and like to play video games</p>
         </div>
