c6900e7aa2f4909603f573d38d52aab3db411cd8
krishan2005op <krishanmalhotra2005@gmail.com>
2024-06-30 13:25:05 +0530

    updated

--- react-app-4/src/App.jsx
+++ react-app-4/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// props = read-only properties that are shared between components
+//         A parent component cn send data to child component.
+// <Component key= value />
+import Student from "./students";
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Student name="spongebob" age = {30} isStudent={true}/>
+      <Student name="Patrick" age = {42} isStudent={false}/>
+      <Student name="squidward" age = {50} isStudent={false}/>
+      <Student name="sandy" age = {27} isStudent={true}/>
     </>
-  )
+  );
 }
 
 export default App
