30293a5256060dd207a44ad5cbef7ef7044036ad
krishan2005op <krishanmalhotra2005@gmail.com>
2024-07-05 18:09:12 +0530

    updated

---
+++ react-app-6/src/List.jsx
@@ -0,0 +1,12 @@
+//rendering list 
+
+function List(){
+
+    const fruits = ["apple","orange","banana","coconut","pineapple"];
+
+    const listItems = fruits.map()
+
+    return(fruits)
+}
+
+export default List
