fb386461a5dff1edc855595a6cf1a4f6f558adcd
krishan2005op <krishanmalhotra2005@gmail.com>
2024-07-05 19:01:29 +0530

    updated

--- react-app-6/src/List.jsx
+++ react-app-6/src/List.jsx
@@ -1,12 +1,29 @@
+/* eslint-disable react/jsx-key */
 //rendering list 
+// there should be unique key in react items so we can easily track itmes inserted and removed
 
-function List(){
+function List(prop){
 
-    const fruits = ["apple","orange","banana","coconut","pineapple"];
+    const Itemlist = prop.items
+    const category = prop.category
 
-    const listItems = fruits.map()
+    // fruits.sort((a,b)=>
+    //     a.name.localeCompare(b.name)
+    // ); // ALPHABETICAL
 
-    return(fruits)
+    // fruits.sort((a,b)=>
+    //     b.name.localeCompare(a.name)
+    // ); //reverse alphabetical
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    
+    const listItems = Itemlist.map(item  => <li key={item.id}>{item.name} : &nbsp; <b>{item.calories}</b></li>);
+    // const listItems1 = lowCalFruits.map(lowCalFruit  => <li key={lowCalFruit.id}>{lowCalFruit.name} : &nbsp; <b>{lowCalFruit.calories}</b></li>);
+
+    return(<>
+                <h3 className="list-category">{category}</h3>
+                <ol className="list-items">{listItems}</ol>
+          </>)
 }
 
 export default List
