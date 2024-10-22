6ed7f1ffcd5cbe718fe378ccfb9cdde923e3a207
krishan2005op <krishanmalhotra2005@gmail.com>
2024-10-22 15:05:19 +0530

    new

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,33 +1,38 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/navbar'
+import Home from './components/home'
+import Login from './components/Login'
+import About from './components/About'
+import User from './components/user'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar/><Home/></>
+    },
+    {
+      path: "/login",
+      element: <><Navbar/><Login/></>
+    },
+    {
+      path: "/About",
+      element: <><Navbar/><About/></>
+    },
+    {
+      path: "/user/:username",
+      element: <><Navbar/><User/></>
+    },
+  ])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ ReactRouter/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    return(
+    <>
+        <div>
+            <nav>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/About"><li>About</li></Link>
+                <Link to="/Login"><li>Login</li></Link>
+            </nav>
+        </div>
+    </>
+    )
+}
+
+export default Navbar;
